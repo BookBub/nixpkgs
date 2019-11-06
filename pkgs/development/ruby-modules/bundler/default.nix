ac4f96b1c37bc5506d594ef8e5b87941a2f567be
Cara McCormack <cara@bookbub.com>
2019-11-06 17:01:43 -0500

    real real sha?

--- pkgs/development/ruby-modules/bundler/default.nix
+++ pkgs/development/ruby-modules/bundler/default.nix
@@ -5,7 +5,7 @@ buildRubyGem rec {
   name = "${gemName}-${version}";
   gemName = "bundler";
   version = "2.0.2";
-  source.sha256 = "0zdh2cqsnvlyzwx22dbizfjal1b98ibcx7jxm5c2j2ydq68shqni";
+  source.sha256 = "1l5203dhf51dnchrd2lxlvqgikx763jz323imhrbhwm0x3yf2ajc";
   dontPatchShebangs = true;
 
   postFixup = ''
