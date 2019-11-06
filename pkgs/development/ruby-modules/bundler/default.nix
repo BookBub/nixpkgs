eef0c295abaa9db15ce33488fdc7ae25a885ad82
Cara McCormack <cara@bookbub.com>
2019-11-06 16:19:04 -0500

    bundler v 2.0.2 by default

--- pkgs/development/ruby-modules/bundler/default.nix
+++ pkgs/development/ruby-modules/bundler/default.nix
@@ -4,7 +4,7 @@ buildRubyGem rec {
   inherit ruby;
   name = "${gemName}-${version}";
   gemName = "bundler";
-  version = "1.17.3";
+  version = "2.0.2";
   source.sha256 = "0ln3gnk7cls81gwsbxvrmlidsfd78s6b2hzlm4d4a9wbaidzfjxw";
   dontPatchShebangs = true;
 
