86ec876bcbfc7dd4c020b13d54bc966f98dfd154
Cara McCormack <cara@bookbub.com>
2021-01-22 14:44:27 -0600

    hey kids spelling is fun

--- pkgs/tools/admin/aws-vault/default.nix
+++ pkgs/tools/admin/aws-vault/default.nix
@@ -24,7 +24,7 @@ buildGoModule rec {
 
   subPackages = [ "." ];
 
-  buildInuputs = if stdenv.isDarwin then [ undmg ] else [];
+  buildInputs = if stdenv.isDarwin then [ undmg ] else [];
 
   # set the version. see: aws-vault's Makefile
   buildFlagsArray = ''
